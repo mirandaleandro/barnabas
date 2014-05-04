99fc0c55cae7316221f91d51e33bca18ec04fcfa
Leandro <mirandaleandro@gmail.com>
2014-05-04 00:52:53 -0600

    page 404

---
+++ app/service/Global.scala
@@ -0,0 +1,23 @@
+package service
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: leo
+ * Date: 5/4/14
+ * Time: 12:37 AM
+ * To change this template use File | Settings | File Templates.
+ */
+import play.api._
+import play.api.mvc._
+import play.api.mvc.Results._
+import concurrent.Future
+
+object Global extends GlobalSettings {
+
+  override def onHandlerNotFound(request: RequestHeader): Future[SimpleResult] = {
+    Future.successful(NotFound(
+      views.html.errors.notFound(request)
+    ))
+  }
+
+}
