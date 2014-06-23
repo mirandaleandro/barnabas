f4eaabbf3d0e0153ee68bc77a55b9ab1c80c4e3d
Leandro <mirandaleandro@gmail.com>
2014-06-22 23:58:41 -0600

    Heorku

--- app/models/PostgresConnection.scala
+++ app/models/PostgresConnection.scala
@@ -9,11 +9,27 @@ import net.fwbrasil.activate.ActivateContext
 
 object PostgresConnection extends ActivateContext
 {
+ // override val storage = new TransientMemoryStorage
+
   val storage = new PooledJdbcRelationalStorage {
     val jdbcDriver = "org.postgresql.Driver"
-    val user = "postgres"
-    val password = "socrates"
-    val url = "jdbc:postgresql://127.0.0.1/barnabas"
+    val user = "kavafnpqoymcry"
+    val password = "FZOKl0ExcsqMgCRkxbyWK1_CHR"
+    val url = "postgres://kavafnpqoymcry:FZOKl0ExcsqMgCRkxbyWK1_CHR@ec2-54-204-21-178.compute-1.amazonaws.com:5432/dbc4t576sli53u"
     val dialect = postgresqlDialect
   }
 }
+
+/*
+HEROKU
+
+Host	ec2-54-204-21-178.compute-1.amazonaws.com
+Database	dbc4t576sli53u
+User	kavafnpqoymcry
+Port	5432
+Password	Hide FZOKl0ExcsqMgCRkxbyWK1_CHR
+Psql	 heroku pg:psql --app heroku-postgres-e45eb3c0 rose
+URL	Hide postgres://kavafnpqoymcry:FZOKl0ExcsqMgCRkxbyWK1_CHR@ec2-54-204-21-178.compute-1.amazonaws.com:5432/dbc4t576sli53u
+
+
+*/
