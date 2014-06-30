28e7a0e59252eaab51d4f5704d7ef83bfe933cf9
Leandro <mirandaleandro@gmail.com>
2014-06-30 00:03:31 -0600

    Heroku

--- app/models/PostgresConnection.scala
+++ app/models/PostgresConnection.scala
@@ -1,19 +1,18 @@
 // CODE FOR HEROKU DEPLOYMENT
 package models
 
-import net.fwbrasil.activate.ActivateContext
 import net.fwbrasil.activate.storage.relational.PooledJdbcRelationalStorage
 import net.fwbrasil.activate.storage.relational.idiom.postgresqlDialect
-import net.fwbrasil.activate.storage.memory.TransientMemoryStorage
 import net.fwbrasil.activate.ActivateContext
 
 object PostgresConnection extends ActivateContext
 {
+
   val storage = new PooledJdbcRelationalStorage {
     val jdbcDriver = "org.postgresql.Driver"
-    val user = "postgres"
-    val password = "socrates"
-    val url = "jdbc:postgresql://127.0.0.1/barnabas"
+    val user = "kavafnpqoymcry"
+    val password = "FZOKl0ExcsqMgCRkxbyWK1_CHR"
+    val url = "jdbc:postgresql://ec2-54-204-21-178.compute-1.amazonaws.com/dbc4t576sli53u"
     val dialect = postgresqlDialect
   }
 }
